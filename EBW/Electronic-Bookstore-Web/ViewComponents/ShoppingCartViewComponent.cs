0d74d9e0e4ab6adc1d562bb7877285bedd68438e
Vladyslav Maksymenko <maksymenko.vladyslav.o@hneu.net>
2023-04-22 00:12:01 +0300

    Update _Layount View add ViewComponent for HttpContext

---
+++ EBW/Electronic-Bookstore-Web/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using EBW.DataAccess;
+using EBW.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Electronic_Bookstore_Web.ViewComponents
+{
+    public class ShoppingCartViewComponent :ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userClaim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if(userClaim != null) 
+            {
+                if (HttpContext.Session.GetInt32(Status.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(Status.SessionCart, _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == userClaim.Value).GetAwaiter().GetResult().Count());
+                }
+                return View(HttpContext.Session.GetInt32(Status.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+            
+        }
+    }
+}
