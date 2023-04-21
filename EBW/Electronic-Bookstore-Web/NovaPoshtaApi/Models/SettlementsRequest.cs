567fc32fcfe11d2930aaa7aa0f59c4cc32e4a92b
Vladyslav Maksymenko <maksymenko.vladyslav.o@hneu.net>
2023-04-22 00:42:27 +0300

    Delete NovaPoshtaAPI

--- EBW/Electronic-Bookstore-Web/NovaPoshtaApi/Models/SettlementsRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Electronic_Bookstore_Web
-{
-    public class SettlementsRequest
-    {
-        public string CityName { get; set; }
-        public string Limit { get; set; }
-        public string Page { get; set; }
-    }
-}
